acd7a7128b080ce8d4a1e10b49486ca47b617e12
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-09-29 16:02:09 -0700

    adding retirement table code

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -57,3 +57,10 @@ CREATE TABLE dept_emp (
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
+
+-- Retirement eligibility
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
