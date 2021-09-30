6ab7167156f3a9ba86d437530807c32463f0325f
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-09-29 18:57:52 -0700

    update

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -64,3 +64,75 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name,
+	dept_emp.to_date
+	FROM retirement_info LEFT JOIN dept_emp ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- using aliases
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- dept_no_count
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO dept_no_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+
+
+SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary, de.to_date
+into emp_info
+from employees as e
+inner join salaries as s
+on (e.emp_no = s.emp_no)
+inner join dept_emp as de
+on (e.emp_no = de.emp_no)
+where (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	and (e.hire_date BETWEEN '1985-01-01' and '1988-12-31')
+	and (de.to_date = '9999-01-01');
+	
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
