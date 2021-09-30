1119564dcb8710dd038960d8563f5623c7704b66
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-09-29 20:13:21 -0700

    updating schema

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -43,8 +43,7 @@ CREATE TABLE titles (
 	title VARCHAR(16) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 -- creating table department_emp
