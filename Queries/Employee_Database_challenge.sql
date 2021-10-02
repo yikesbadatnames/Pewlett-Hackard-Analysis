3afd14bfd7f9a62631c48582c2444b19d673d0d5
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-10-01 20:22:16 -0700

    deliverable 1.1

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -16,3 +16,21 @@ ______
 INTO nameyourtable
 FROM _______
 ORDER BY _____, _____ DESC;
+
+--deliverable 1
+select distinct e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+into retirement_titles
+from employees as e 
+left join titles as t 
+on (e.emp_no = t.emp_no)
+where e.birth_date between '1952-01-01' and '1955-12-31'
+order by e.emp_no;
+
+--deliverable1.1
+select distinct on(e.emp_no) e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+into unique_titles
+from employees as e 
+left join titles as t 
+on (e.emp_no = t.emp_no)
+where e.birth_date between '1952-01-01' and '1955-12-31'
+order by e.emp_no;
