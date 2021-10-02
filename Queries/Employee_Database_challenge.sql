a389ab408df84c00baa24bd004c3b84f5913a4f8
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-10-01 21:14:45 -0700

    rmv wrng csv add 1.2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -34,3 +34,15 @@ left join titles as t
 on (e.emp_no = t.emp_no)
 where e.birth_date between '1952-01-01' and '1955-12-31'
 order by e.emp_no, to_date desc;
+
+--- deliverable 1.1
+Select count, title 
+into retiring_titles
+From
+(select distinct on(t.title) count(e.emp_no) as count, t.title
+from employees as e 
+left join unique_titles as t 
+on (t.emp_no = e.emp_no)
+where e.birth_date between '1952-01-01' and '1955-12-31'
+group by t.title) as x
+order by count  desc;
