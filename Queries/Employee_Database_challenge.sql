c8cf3e61ac64ebc0bf52d26ab42105763f00b235
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-10-01 20:49:00 -0700

    corrected 1.1

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -33,4 +33,4 @@ from employees as e
 left join titles as t 
 on (e.emp_no = t.emp_no)
 where e.birth_date between '1952-01-01' and '1955-12-31'
-order by e.emp_no;
+order by e.emp_no, to_date desc;
