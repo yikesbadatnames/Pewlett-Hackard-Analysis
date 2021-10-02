6be57cd1dab8bc79020fc56637d28010557c2458
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-10-02 10:21:56 -0700

    deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -46,3 +46,14 @@ on (t.emp_no = e.emp_no)
 where e.birth_date between '1952-01-01' and '1955-12-31'
 group by t.title) as x
 order by count  desc;
+
+
+--- deliverable 2
+Select distinct on(e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+into mentorship_eligibility
+from employees as e
+left join dept_emp as de
+on (e.emp_no = de.emp_no)
+left join titles as t
+on (de.emp_no = t.emp_no)
+where (de.to_date = '9999-01-01') and (e.birth_date between '1965-01-01' and '1965-12-31');
